d1e3c82af5ca21872d5ae8001137eed1df61a416
anryko <a.shvenke@gmail.com>
2015-08-24 22:41:41 +0200

    Added Client Src IP default respond.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(ip_addr)]
+
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    let ip = stream.peer_addr().unwrap().ip().to_string() + "\n";
+    let _ = stream.write(ip.as_bytes());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => { println!("failed: {}", e) }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+        }
+    }
 }
